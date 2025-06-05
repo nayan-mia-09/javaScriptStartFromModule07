fc203544da735f422c41e50b46abbc4217827c12
Nayan <dreamerboynayan@gmail.com>
2025-06-05 09:18:10 +0600

    Comparision

---
+++ Comparision.js
@@ -0,0 +1,38 @@
+//1
+const imEating = 7;
+const brotherEating = 5;
+
+console.log(imEating > brotherEating);
+
+//2
+const myClassStudent = 45;
+const friendClassStudent = 50;
+
+console.log(myClassStudent < friendClassStudent);
+
+//3
+const myMarks = 75;
+const friendMarks = 75;
+
+console.log(myMarks === friendMarks);
+
+//4
+const target = 100;
+const save = 95;
+
+console.log(target <= save);
+
+//5
+const num1 = 10;
+const num2 = 7;
+
+console.log(num1 != num2);
+
+//6
+console.log(20 <= 15);
+
+//7
+const myResult = 45;
+const friendResult = 97;
+
+console.log(friendResult < myResult);
