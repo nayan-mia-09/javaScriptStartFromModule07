a7ef210967975c917acc6e2c75c71f6154ddfbd0
Nayan <dreamerboynayan@gmail.com>
2025-06-05 06:00:58 +0600

    Variable

---
+++ Variable.js
@@ -0,0 +1,10 @@
+const myAge = 22;
+const sibling = 2;
+const students = 80;
+const marks = 2.82;
+const rooms = 3;
+const readingTime = 8;
+const NumDigit = 0 + 1 + 7 + 7 + 8 + 1 + 3 + 0 + 9 + 6 + 5;
+const result = NumDigit;
+
+console.log(myAge, sibling, students, marks, rooms, readingTime, result);
