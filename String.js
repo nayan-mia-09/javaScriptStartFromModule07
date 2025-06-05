2d594c6759754156b398ec27d9b33b4cb564627b
Nayan <dreamerboynayan@gmail.com>
2025-06-05 06:18:36 +0600

    String

---
+++ String.js
@@ -0,0 +1,27 @@
+//1
+const name = "Nayan Mia";
+console.log(name);
+
+//2
+const address = "Sadarpur,Faridpur";
+console.log(address);
+
+//3
+const mitusVersity = "Chittagong University";
+console.log(mitusVersity);
+
+//4
+const fan = "Hania Amir";
+console.log(fan);
+
+//5
+const movieOfTheYear = "Borbad";
+console.log(movieOfTheYear);
+
+//6
+const hobby = "Car Drive";
+console.log(hobby);
+
+//7
+const color = "black and white";
+console.log(color);
