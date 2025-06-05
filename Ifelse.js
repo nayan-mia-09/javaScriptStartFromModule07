b4fca4c4b6403fcae41017b87897f58c497a0b3b
Nayan <dreamerboynayan@gmail.com>
2025-06-05 14:45:37 +0600

    If else 2

--- Ifelse.js
+++ Ifelse.js
@@ -52,3 +52,94 @@ if(temp >= 30){
 }else{
     console.log("Kombol Muri diye Ghum!");
 }
+
+
+// 01
+let age = 19;
+let height = 60;
+
+if(age > 18 && height > 60){
+    console.log("Gari Dhakka Dibi!");
+}else{
+    console.log("Bose Thakbi!");
+}
+
+// 02
+let mathMark = 75 ;
+let englishMark = 80;
+
+if(mathMark > 80 && englishMark > 85){
+    console.log("You are eligible");
+}else{
+    console.log("Toke Biye diye Dibo!");
+}
+
+//03
+let GPA = 5;
+let monthlyIncome = 9000;
+
+if(GPA === 5 && monthlyIncome < 10000){
+    console.log("Scholorship Pabi!");
+}else{
+    console.log("Beton Diye Porte Hobe!");
+}
+
+//04
+let ifAge = 30;
+let isExperience = 3;
+
+if(ifAge <= 30 && isExperience > 2){
+    console.log("Exam Dite Parbi!");
+}else{
+    console.log("Exam Dite Parbi Na!");
+}
+
+//05
+let hasEggNum = 15;
+let hasChiken = false;
+
+if(hasEggNum > 12 || hasChiken === false){
+    console.log("Dimer Korma Ranna Hobe!");
+}else{
+    console.log("Kola ruti khete hobe");
+}
+
+//06
+let bodyTemp = 98;
+let hasCaugh = true;
+
+if(bodyTemp > 100 || hasCaugh === true){
+    console.log("Go to Doctor!");
+}else{
+    console.log("Katha Muri Diye Ghuma!");
+}
+
+//07
+let attendence = 80;
+let submitHomeWork = true;
+
+if(attendence > 80 && submitHomeWork === true){
+    console.log("Exam Dite Parbi!");
+}else{
+    console.log("Auto Fail!");
+}
+
+//08
+let hasBiddut = false;
+let mobileCharged = true;
+
+if(hasBiddut === false && mobileCharged === false){
+    console.log("Porte Boshbi!");
+}else{
+    console.log("Game Khelbi!");
+}
+
+//09
+let shirtPrice = 800;
+let hasKuppon = true;
+
+if(shirtPrice > 1000 && hasKuppon === true){
+    console.log("Discount Pabi!");
+}else{
+    console.log("Full Payment Korte Hobe!");
+}
