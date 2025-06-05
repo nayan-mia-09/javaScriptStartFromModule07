f69ec7a557b274365b5ad5bd74af6b5804b5e46c
Nayan <dreamerboynayan@gmail.com>
2025-06-05 06:11:42 +0600

    Variable P2

--- Variable.js
+++ Variable.js
@@ -1,10 +1,39 @@
-const myAge = 22;
-const sibling = 2;
-const students = 80;
-const marks = 2.82;
-const rooms = 3;
-const readingTime = 8;
-const NumDigit = 0 + 1 + 7 + 7 + 8 + 1 + 3 + 0 + 9 + 6 + 5;
-const result = NumDigit;
+// const myAge = 22;
+// const sibling = 2;
+// const students = 80;
+// const marks = 2.82;
+// const rooms = 3;
+// const readingTime = 8;
+// const NumDigit = 0 + 1 + 7 + 7 + 8 + 1 + 3 + 0 + 9 + 6 + 5;
+// const result = NumDigit;
 
-console.log(myAge, sibling, students, marks, rooms, readingTime, result);
+// console.log(myAge, sibling, students, marks, rooms, readingTime, result);
+
+
+// 1
+const age = 22;
+console.log(age);
+
+//2
+const booksToRead = 10;
+console.log(booksToRead);
+
+//3
+const wakeUpTime = 6;
+console.log(wakeUpTime);
+
+//4
+const wastedTime = 3;
+console.log(wastedTime);
+
+//5
+const borrow = 50000;
+console.log(borrow);
+
+//6
+const goals = 2;
+console.log(goals);
+
+//7
+const pillow = 1;
+console.log(pillow);
