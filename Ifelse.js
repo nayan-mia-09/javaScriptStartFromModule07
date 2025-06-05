79afc50668a896110f676ea4dff8151060fc69c8
Nayan <dreamerboynayan@gmail.com>
2025-06-05 14:20:24 +0600

    IF Else

---
+++ Ifelse.js
@@ -0,0 +1,54 @@
+
+//1
+let backToHome = 6;
+
+if(backToHome <= 6 ){
+    console.log("Tor Jonno Nodoles Ranna korchi!");
+}else{
+    console.log("Tor Jonno Jharur Bari Ache!");
+}
+
+//2
+let mobileON = true;
+
+if(mobileON == true){
+    console.log("Welcome!");
+}else{
+    console.log("Get Lost!");
+}
+
+//3
+let run = 3;
+
+if(run === 5){
+    console.log("Toke Chocolate Dibo!");
+}else{
+    console.log("Tor Mota Vuri Hobe!");
+}
+
+//4
+let result = 85;
+
+if(result > 80){
+    console.log("Bike Pabi!");
+}else{
+    console.log("Bike Pabi Na!");
+}
+
+//5
+let movieShow = 9;
+
+if(movieShow < 9){
+    console.log("Movie Dekhbo!");
+}else{
+    console.log("Nake Tel Diye Ghumabo!");
+}
+
+//6
+let temp = 32;
+
+if(temp >= 30){
+    console.log("AC ON");
+}else{
+    console.log("Kombol Muri diye Ghum!");
+}
