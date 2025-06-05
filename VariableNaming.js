02c2ebc0cb0558d4c9dca6604451cd28c3bf62db
Nayan <dreamerboynayan@gmail.com>
2025-06-05 07:42:39 +0600

    Variable Naming

---
+++ VariableNaming.js
@@ -0,0 +1,40 @@
+//1
+// Using variable name in camelCase
+const firstName = "Nayan Mia"; 
+console.log(firstName);
+
+//2
+// Using case-sensitive variable
+
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city,);
+console.log(City);
+
+//3
+//Using case-sensitive variable
+const userName = "Johan";
+const username = "Jovan";
+
+console.log(userName);
+console.log(username);
+
+//4
+//Using variable name in camelCase
+const myAddress = "Sadarpur, Faridpur";
+console.log(myAddress);
+
+//5
+// SyntaxError
+// const super = 25;
+// console.log(super);
+
+//6
+// SyntaxError
+// const class = 10;
+// console.log(class);
+
+//7
+// SyntaxError
+// const 2ndPosition = "T.Sakib"
+// console.log(2ndPosition);
