15a8a259247d5f2f49244fbd435cee014cb66edd
Nayan <dreamerboynayan@gmail.com>
2025-06-05 07:28:32 +0600

    How to Comment

---
+++ Commenting.js
@@ -0,0 +1,43 @@
+//1
+// Bangladesh vs Bhutan Fifa Friendly football match score
+const score = 2;
+console.log(score);
+
+//2
+// Radius of a PI
+const radius = 3.1416;
+console.log(radius);
+
+//3
+// My Favourite Dress number
+const dress = 5;
+console.log(dress);
+
+//4
+// Gift for my result
+const giftMoney = 100;
+console.log(giftMoney);
+
+//5
+// Today I'm studying 8 hours
+// I'm Learning JavaScript
+// I'm practicing
+// I'm Reading Books
+// I'm Watching English Speaking practice video
+
+const studyTime = 8;
+console.log(studyTime);
+
+//6
+// I'm sending money to  my friend 
+// I'm sending 100 tk for eid salami
+// No service charge for send money
+
+const sendMoney = 100;
+console.log(sendMoney);
+
+//7
+// Included product price and delivery charge 
+
+const pay = 1028;
+console.log(pay);
