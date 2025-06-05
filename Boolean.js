0c18cee6bb70cb90998559b016210cbc81cad847
Nayan <dreamerboynayan@gmail.com>
2025-06-05 06:28:05 +0600

    Boolean

---
+++ Boolean.js
@@ -0,0 +1,30 @@
+//1
+const isHappy = false;
+console.log(isHappy);
+
+//2
+const crushMessage = false;
+console.log(crushMessage);
+
+//3
+const celebrating = true;
+console.log(celebrating);
+
+//4
+const IsHappy = true;
+const hasMoney = false;
+const isHungry = true;
+
+console.log(IsHappy, hasMoney, isHungry);
+
+//5
+const isStudyDone = false;
+console.log(isStudyDone);
+
+//6
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+//7
+const isDinnerDone = true;
+console.log(isDinnerDone);
