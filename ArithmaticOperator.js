e0bb349a69cb97c64568a403bf37114a45a40458
Nayan <dreamerboynayan@gmail.com>
2025-06-05 06:54:37 +0600

    Arithmatic Operator

---
+++ ArithmaticOperator.js
@@ -0,0 +1,75 @@
+//1
+const earnMoney = 100;
+const costMoney = 40;
+
+const hadMoney = earnMoney - costMoney;
+console.log(hadMoney); // 60
+
+//2
+const hasPencil = 10;
+const everyTimeUse = 2;
+
+const totalUsingTime = hasPencil / everyTimeUse; 
+console.log(totalUsingTime); // 5
+
+//3
+const hasMoney = 60;
+const eachTimeUse = 15;
+
+const totalTime = 60 / 15;
+const afterCostMoney = 60 % 15; 
+console.log(totalTime, afterCostMoney); //4, 0
+
+//4
+const text1 = "Hello";
+const text2 = "World";
+
+const textConcat = text1 +text2;
+console.log(textConcat); // HelloWorld
+
+//5
+const buyRice = 153;
+const buyLentil = 261;
+
+const totalBuy = buyLentil + buyRice;
+console.log(totalBuy); // 414
+
+//6
+const moneyHave = 500;
+const costEachTime = 75;
+
+const totalCostTime = 500 / 75;
+const afterCostRemainder = 500 % 75;
+console.log(totalCostTime, afterCostRemainder);
+
+//7
+const num1 = 8;
+const num2 = 3;
+
+const numDivide = num1 / num2;
+const numRemainder = num1 % num2;
+console.log(numDivide, numRemainder); 
+
+//8 
+const num3 = 50;
+const num4 = 9;
+
+const num34Remainder = num3 % num4;
+console.log(num34Remainder);
+
+//9 
+const firstName = "Bangla";
+const lastName = "desh";
+
+const countryName = firstName + lastName;
+console.log(countryName);
+
+//10
+const haveRice = 480;
+const perDayRice = 4;
+const oneMonth = 30;
+
+const howLongDay = haveRice / perDayRice;
+const howLongMonth = haveRice / oneMonth;
+
+console.log(howLongDay, howLongMonth);
