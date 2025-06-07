5b5f3535779a20a6fc05ebcf27f2705cb55fb3fb
Nayan <dreamerboynayan@gmail.com>
2025-06-07 08:43:26 +0600

    Arry

---
+++ Arry.js
@@ -0,0 +1,38 @@
+// 1
+
+const numbers = [71,72,73,74,75,76,77,78,79]
+console.log(numbers);
+console.log(numbers.length);
+
+// 2
+const fruits = [ "Apple", "Banana", "Orange", "Mango", "Guava"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[4]);
+
+// 3
+const Vegetables = ["Potato", "Onion", "Brinjal", "Radish",
+     "Carrot", "Tomato", "Ledis Finger", 
+     "Bottle gourd", "Cucumber", "Pointed gourd"]
+
+console.log(Vegetables.length);   
+
+// 4
+const favMovie = ["3 Idiots", "12th Fail", "Life Partner", "Borbad", "I Love You"]
+console.log(favMovie);
+
+// 5
+
+const oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+console.log(oddNumbers);
+
+// 6
+
+const colors = ["Yellow", "Blue", "White", "Black", "Pink", "Green"];
+console.log(colors.length);
+
+
+// 7
+
+const cityName = ["Dhaka", "Delhi", "Riyad", "Kualalampur","Madrid","Zurich"];
+console.log(cityName.length);
