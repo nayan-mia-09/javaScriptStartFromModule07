c169ca4f9e8c8c380bf8152e5fb02dcd10adc56c
Nayan <dreamerboynayan@gmail.com>
2025-06-09 09:25:26 +0600

    Loop

---
+++ Loop.js
@@ -0,0 +1,34 @@
+// 1
+const subject = ["Bangla", "English", "Math", "Science", "Geography", "Economics", "Political Science"];
+
+for(const sub of subject){
+    console.log(sub);
+}
+
+// 2
+const favFood = ["Rice", "Lentil", "Khichuri", "Briyani", "Desert"];
+
+for(const food of favFood){
+    console.log(food);
+}
+
+// 3
+const familyMembers = [1980, 1978, 1996, 2003];
+
+for(const date of familyMembers){
+    console.log(date);
+}
+
+// 4
+const team = ["Jamal", "Sohel", "Tariq", "Hamza", "Fahmidul", "Rakib", "Morsalin", "Shomit", "Mitul"];
+
+for(const player of team){
+    console.log(player);
+}
+
+// 5
+const examDate = ["Dec 31", "Jan 2", "Jan 5", "Jan 10", "Jan 13", "Jan 17", "Jan 25", "Feb 4"];
+
+for(const date of examDate){
+    console.log(date);
+}
