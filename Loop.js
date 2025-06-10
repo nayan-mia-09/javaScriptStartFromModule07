9b443c11059ec0e216850bed53365164c7995701
Nayan <dreamerboynayan@gmail.com>
2025-06-10 07:56:38 +0600

    For loop with If else condition p2

--- Loop.js
+++ Loop.js
@@ -208,3 +208,54 @@ while(nums <= 40){
         console.log(i);
     }
   }
+
+  // For loop with if condition p2
+
+  // 1
+  for(let i = 1; i < 30; i++){
+    console.log(i);
+    if(i >= 15){
+        break
+    }
+  }
+
+  // 2
+  for(let i = 1; i < 40; i++){
+    if(i % 7 ==0){
+        continue
+    }
+    console.log(i);
+  }
+
+  // 3
+  for(let i = 1; i < 15; i++){
+    if(i == 9){
+        continue
+    }
+    console.log(i);
+  }
+
+  // 4
+  for(let i = 1; i < 20; i++){
+    if(i == 12){
+        continue
+    }
+    console.log(i);
+  }
+
+  // 5
+  for(let i = 1; i < 25; i++){
+    if(i % 3 == 0){
+        continue
+    }
+    console.log(i);
+  }
+
+  // 6
+
+  for(let i = 91; i < 120; i++){
+    if(i % 10 == 0){
+        continue
+    }
+    console.log(i);
+  }
